7bd48c1683d09715700d9b736aa73c4ad6d89dbf
looreenz <lorenzo.cremonese10@gmail.com>
2025-04-08 12:28:21 +0200

    Implement redux-persist for user state management and update main entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from "redux-persist/lib/storage/session";
 import userReducer from "./slices/userSlice";
+
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedUserReducer,
   },
 });
+
+export const persistor = persistStore(store);
